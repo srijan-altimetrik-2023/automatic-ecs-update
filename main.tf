b17c1015dc9a234f3fd1e93b16e746d9d2932aa9
srijan-altimetrik-2023 <137090105+srijan-altimetrik-2023@users.noreply.github.com>
2023-06-27 15:39:26 +0530

    Update main.tf
    sdf

--- main.tf
+++ main.tf
@@ -1,14 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "4.45.0"
-    }
-  }
-}
-
-
-
 # Define the provider and region
 provider "aws" {
   region = "ap-south-1"
