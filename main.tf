e2b5ecef5d262d6b4003b3edcf3ca1db44362923
srijan-altimetrik-2023 <137090105+srijan-altimetrik-2023@users.noreply.github.com>
2023-06-27 15:34:47 +0530

    Update main.tf
    wef

--- main.tf
+++ main.tf
@@ -14,16 +14,20 @@ provider "aws" {
   region = "ap-south-1"
 }
 
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "10.0.0.0/16"
+}
+
 # Create an ECS cluster
-resource "aws_ecs_cluster" "ecs_cluster" {
-  name = "my-ecs-cluster"
+resource "aws_ecs_cluster" "my_cluster" {
+  name = "my-cluster"
 }
 
 # Create an EC2 instance
-resource "aws_instance" "ecs_instance" {
+resource "aws_instance" "my_instance" {
   instance_type = "t2.micro"
   ami           = "ami-0f5ee92e2d63afc18"  # Replace with the desired EC2 instance AMI ID
-  subnet_id     = "subnet-0f149076385f14b45"  # Replace with the desired subnet ID
+  subnet_id     = aws_vpc.my_vpc.id  # Replace with the desired subnet ID
 
   tags = {
       Environment = "Prod"
@@ -34,12 +38,31 @@ resource "aws_instance" "ecs_instance" {
   }
 
   # Attach the ECS cluster instance to the cluster
-  user_data = <<-EOF
-              #!/bin/bash
-              echo ECS_CLUSTER=${aws_ecs_cluster.ecs_cluster.name} >> /etc/ecs/ecs.config
-              EOF
-
-  # Attach a security group to the instance
-  vpc_security_group_ids = ["sg-0b2b80b19887446b4"]  # Replace with the desired security group ID
 }
-
+resource "aws_ecs_task_definition" "my_task_definition" {
+  family                   = "my-task-family"
+  container_definitions    = <<EOF
+  [
+    {
+      "name": "my-container",
+      "image": "nginx",
+      "cpu": 256,
+      "memory": 512,
+      "portMappings": [
+        {
+          "containerPort": 80,
+          "hostPort": 80
+        }
+      ]
+    }
+  ]
+  EOF
+}
+resource "aws_ecs_service" "my_service" {
+  name            = "my-service"
+  cluster         = aws_ecs_cluster.my_cluster.id
+  task_definition = aws_ecs_task_definition.my_task_definition.arn
+  desired_count   = 1
+  
+  # Other service configuration options
+}
