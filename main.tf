98f5e50acb247330fe4cd80bda247e1b40aa10a1
srijan-altimetrik-2023 <137090105+srijan-altimetrik-2023@users.noreply.github.com>
2023-06-28 16:14:48 +0530

    Update main.tf
    sfdfds

--- main.tf
+++ main.tf
@@ -8,11 +8,33 @@ terraform {
     secret_key = "5HHt423zsVJi53SfXebzVfQ5A7yLU9t3WEHGwR+x"
   }
 }
+
 provider "aws" {
   region = "ap-south-1"
 }
 
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+        Environment = "Prod"
+        Application = "Testing"
+        Project = "CloudOps"
+        Owner = "mnageti@altimetrik.com"
+        Name = "tf_my_vpc"
+    }
+}
 
+# Create an ECS cluster
+resource "aws_ecs_cluster" "my_cluster" {
+  name = "my-cluster"
+  tags = {
+        Environment = "Prod"
+        Application = "Testing"
+        Project = "CloudOps"
+        Owner = "mnageti@altimetrik.com"
+        Name = "tf_my_cluster"
+    }
+}
 
 # Create an EC2 instance
 resource "aws_instance" "instance" {
@@ -33,6 +55,47 @@ resource "aws_instance" "instance" {
       Owner = "pawkumar@altimetrik.com"
       Name = "SCP_test"
   }
-  vpc_security_group_ids = ["sg-01e24941edc06d626"]
+  vpc_security_group_ids = sg-01e24941edc06d626
 }
 
+resource "aws_ecs_task_definition" "my_task_definition" {
+  family                   = "my-task-family"
+ tags = {
+      Environment = "Prod"
+      Application = "Testing"
+      Project = "CloudOps"
+      Owner = "mnageti@altimetrik.com"
+      Name = "tf_my_task_definition"
+  }
+  container_definitions    = <<EOF
+  [
+    {
+      "name": "my-container",
+      "image": "nginx",
+      "cpu": 256,
+      "memory": 512,
+      "portMappings": [
+        {
+          "containerPort": 80,
+          "hostPort": 80
+        }
+      ]
+    }
+  ]
+  EOF
+}
+resource "aws_ecs_service" "my_service" {
+  name            = "my-service"
+  cluster         = aws_ecs_cluster.my_cluster.id
+  task_definition = aws_ecs_task_definition.my_task_definition.arn
+  desired_count   = 1
+   tags = {
+      Environment = "Prod"
+      Application = "Testing"
+      Project = "CloudOps"
+      Owner = "mnageti@altimetrik.com"
+      Name = "tf_my_service"
+  }
+  
+  # Other service configuration options
+}
