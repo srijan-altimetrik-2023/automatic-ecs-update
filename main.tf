38d4e487ee396493c1afb877554e6fad8f1aa082
srijan-altimetrik-2023 <137090105+srijan-altimetrik-2023@users.noreply.github.com>
2023-06-28 16:50:42 +0530

    Update main.tf
    removed the aws keys

--- main.tf
+++ main.tf
@@ -4,8 +4,7 @@ terraform {
     bucket     = "ami-automate-s3"
     key        = "state.txt"
     region = "ap-south-1"
-    access_key = "AKIASYQ3HBXP4WF6HPY5"
-    secret_key = "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"
+  
   }
 }
 
