01c018f4eb1c6826f9ca730826a60c18958f7944
srijan-altimetrik-2023 <137090105+srijan-altimetrik-2023@users.noreply.github.com>
2023-07-02 17:12:24 +0530

    Update versions.tf
    removed version

--- versions.tf
+++ versions.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
+backend "s3" {
+    bucket     = "ami-automate-s3"
+    key        = "state.txt"
+    region = "ap-south-1"
   }
+
 }
 provider "aws" {
   region = "ap-south-1"
