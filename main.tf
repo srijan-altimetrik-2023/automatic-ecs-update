48d66d45f933236f115b497852f5aba4ce3eaede
srijan-altimetrik-2023 <137090105+srijan-altimetrik-2023@users.noreply.github.com>
2023-07-02 15:59:22 +0530

    Update main.tf
    fsda

--- main.tf
+++ main.tf
@@ -162,7 +162,7 @@ resource "aws_ecs_service" "hello_world" {
   name            = "hello-world-service"
   cluster         = aws_ecs_cluster.main.id
   task_definition = aws_ecs_task_definition.hello_world.arn
-  desired_count   = var.app_count
+  desired_count   = 1
   launch_type     = "FARGATE"
 
   network_configuration {
